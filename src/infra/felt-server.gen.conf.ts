9787768463b5f4c34ed9f6c6704c3cd97bd6682b
Ryan Atkinson <2608646+ryanatkn@users.noreply.github.com>
2021-10-28 13:21:51 -0500

    fix production nginx config ports (#171)

--- src/infra/felt-server.gen.conf.ts
+++ src/infra/felt-server.gen.conf.ts
@@ -1,6 +1,6 @@
 import type {Gen} from '@feltcoop/gro/dist/gen/gen.js';
 
-import {DEPLOY_SERVER_HOST, API_SERVER_HOST, SVELTEKIT_SERVER_HOST} from '$lib/config';
+import {DEPLOY_SERVER_HOST, API_SERVER_HOST_PROD, SVELTEKIT_SERVER_HOST} from '$lib/config';
 
 // Outputs an nginx config with configured values.
 export const gen: Gen = async () => {
@@ -10,7 +10,7 @@ server {
   server_name ${DEPLOY_SERVER_HOST};
 
   location /api {
-    proxy_pass http://${API_SERVER_HOST};
+    proxy_pass http://${API_SERVER_HOST_PROD};
   }
 
   location /ws {
@@ -19,7 +19,7 @@ server {
     proxy_http_version 1.1;
     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
     proxy_set_header Host $host;
-    proxy_pass http://${API_SERVER_HOST};
+    proxy_pass http://${API_SERVER_HOST_PROD};
   }
 
   location / {
--- src/lib/config.js
+++ src/lib/config.js
@@ -1,18 +1,24 @@
 // this is a JS file so it can be imported by `svelte.config.js`
 
-// TODO figure out how to allow user projects to override these vars
+// TODO This module is going to change significantly:
+// - figure out how to allow user projects to override these vars
+// - it should not be imported on the client, instead the values should be replaced at build time
 
 // TODO Replace this at buildtime with a constant so it can be optimized.
 // This probably means supporting `import.meta.env` or `$app/env` in Gro.
 const dev = import.meta.env?.DEV ?? process.env.NODE_ENV !== 'production';
 
-export const SVELTEKIT_SERVER_HOST = 'localhost:3000';
-export const API_SERVER_PORT = dev ? 3001 : 3000; // TODO maybe use `process.env.PORT`?
-export const API_SERVER_HOST = `localhost:${API_SERVER_PORT}`;
-export const WEBSOCKET_URL = dev
-	? `ws://localhost:${API_SERVER_PORT}/ws`
-	: 'wss://staging.felt.dev/ws';
-
-export const DEPLOY_SERVER_HOST = 'felt.dev';
+export const DEPLOY_SERVER_HOST = 'staging.felt.dev';
 export const DEPLOY_IP = '96.126.116.174';
 export const DEPLOY_USER = 'root';
+
+export const SVELTEKIT_SERVER_HOST = 'localhost:3000';
+export const API_SERVER_PORT_DEV = 3001;
+export const API_SERVER_PORT_PROD = 3000;
+export const API_SERVER_PORT = dev ? API_SERVER_PORT_DEV : API_SERVER_PORT_PROD; // TODO maybe use `process.env.PORT`?
+export const API_SERVER_HOST_DEV = `localhost:${API_SERVER_PORT_DEV}`;
+export const API_SERVER_HOST_PROD = `localhost:${API_SERVER_PORT_PROD}`;
+export const API_SERVER_HOST = dev ? API_SERVER_HOST_DEV : API_SERVER_HOST_PROD;
+export const WEBSOCKET_URL_DEV = `ws://localhost:${API_SERVER_PORT_DEV}/ws`;
+export const WEBSOCKET_URL_PROD = `wss://${DEPLOY_SERVER_HOST}/ws`;
+export const WEBSOCKET_URL = dev ? WEBSOCKET_URL_DEV : WEBSOCKET_URL_PROD;
