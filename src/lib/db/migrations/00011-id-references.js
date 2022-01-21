b14127768165447f0bab6087d0f966ef5670fb7e
Ryan Atkinson <2608646+ryanatkn@users.noreply.github.com>
2022-01-21 12:37:20 -0700

    fix two db constraints (#220)

---
+++ src/lib/db/migrations/00011-id-references.js
@@ -0,0 +1,21 @@
+/** @param {import('postgres').Sql<any>} sql */
+export const up = async (sql) => {
+	// Set persona.account_id foreign key.
+	await sql`
+		ALTER TABLE personas
+		ADD CONSTRAINT personas_account_id_fkey
+			FOREIGN KEY (account_id)
+			REFERENCES accounts (account_id)
+			ON UPDATE CASCADE ON DELETE CASCADE;
+	`;
+
+	// Make membership cascade on delete community_id.
+	await sql`
+		ALTER TABLE memberships
+		DROP CONSTRAINT memberships_community_id_fkey,
+		ADD CONSTRAINT memberships_community_id_fkey
+			FOREIGN KEY (community_id)
+			REFERENCES communities (community_id)
+			ON UPDATE CASCADE ON DELETE CASCADE;
+	`;
+};
