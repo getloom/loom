993280a8b1bcd6401e9118796587b93d12c245b7
Ryan Atkinson <2608646+ryanatkn@users.noreply.github.com>
2022-04-01 13:14:34 -0600

    properly count graphemes when validating space icons (#305)

--- src/lib/vocab/space/spaceHelpers.ts
+++ src/lib/vocab/space/spaceHelpers.ts
@@ -1,12 +1,18 @@
 import type {Result} from '@feltcoop/felt';
+import {toGraphemeCount} from '@feltcoop/felt/util/string.js';
 
 export const ICON_MISSING_ERROR = 'please add an icon, any character including emoji';
 export const ICON_TOO_LONG_ERROR = 'icon must be exactly 1 character';
 
-// TODO look into using ajv or zod for parsing
+// TODO move to schema for server-side validation -- `maxLength` won't work because it uses `.length`
+const ICON_MAX_LENGTH = 1;
+
 export const parseSpaceIcon = (value: string): Result<{value: string}, {message: string}> => {
-	if (!value) return {ok: false, message: ICON_MISSING_ERROR};
-	const length = [...value].length;
-	if (length > 1) return {ok: false, message: ICON_TOO_LONG_ERROR};
+	if (!value) {
+		return {ok: false, message: ICON_MISSING_ERROR};
+	}
+	if (toGraphemeCount(value) > ICON_MAX_LENGTH) {
+		return {ok: false, message: ICON_TOO_LONG_ERROR};
+	}
 	return {ok: true, value};
 };
