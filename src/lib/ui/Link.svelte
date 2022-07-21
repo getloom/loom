a91ce5d6f0c15fa656369511500ef5d4ad2775b5
Ryan Atkinson <mail@ryanatkn.com>
2022-07-21 10:10:53 -0500

    add target for external links (#430)

--- src/lib/ui/Link.svelte
+++ src/lib/ui/Link.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
 	export let href: string;
+
+	$: external = !(href.startsWith('/') || href.startsWith('.'));
+	$: rel = external ? 'external noreferrer nofollow' : undefined;
+	$: target = external ? '_blank' : undefined;
+	$: prefetch = external ? undefined : (true as const);
 </script>
 
-<a rel="noreferrer" {...$$restProps} {href}><slot /></a>
+<a {...$$restProps} {href} {rel} {target} sveltekit:prefetch={prefetch}><slot /></a>
