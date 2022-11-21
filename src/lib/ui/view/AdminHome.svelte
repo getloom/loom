9d6051c5f503a4af51b1fbf2521324f47ba83bbb
Ryan Atkinson <mail@ryanatkn.com>
2022-11-21 09:51:09 -0600

    add persona and community explorer components (#580)

--- src/lib/ui/view/AdminHome.svelte
+++ src/lib/ui/view/AdminHome.svelte
@@ -1,24 +1,8 @@
 <script lang="ts">
-	import {getApp} from '$lib/ui/app';
-	import type {Community} from '$lib/vocab/community/community';
-	import Avatar from '$lib/ui/Avatar.svelte';
-	import {getViewContext} from '$lib/vocab/view/view';
 	import RolesList from '$lib/ui/RolesList.svelte';
 	import Todo from '$lib/ui/view/Todo.svelte';
-
-	const {dispatch} = getApp();
-
-	const viewContext = getViewContext();
-	$: ({persona} = $viewContext);
-
-	// TODO this is just stubbing out some admin-like UI
-	let communities: Community[] | undefined;
-	const loadAllCommunities = async () => {
-		// TODO need to cache this data in the `ui` somehow -- see comment below
-		const result = await dispatch.ReadCommunities({actor: $persona.persona_id});
-		if (!result.ok) throw Error(); // TODO querying helpers
-		communities = result.value.communities;
-	};
+	import PersonaExplorer from '$lib/ui/PersonaExplorer.svelte';
+	import CommunityExplorer from '$lib/ui/CommunityExplorer.svelte';
 </script>
 
 <div class="padded-xl">
@@ -40,19 +24,10 @@
 		<Todo />
 	</section>
 	<section>
-		<h2>communities</h2>
-		{#if communities}
-			<ul>
-				{#each communities as community}
-					<!-- TODO `CommunityAvatar` expects a store that's registered with the system for the contextmenu,
-        and we probably want that as well, right? So how can we cache the data? See above. -->
-					<Avatar name={community.name} type="Community" hue={community.settings.hue} />
-				{/each}
-			</ul>
-		{:else}
-			<!-- TODO pending status with query state -->
-			<button on:click={loadAllCommunities}>load all communities</button>
-		{/if}
+		<CommunityExplorer />
+	</section>
+	<section>
+		<PersonaExplorer />
 	</section>
 </div>
 
