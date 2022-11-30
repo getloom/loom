818db951dd0067722995a90ddd9655ea112ddaf9
greatbacon <4512607+greatbacon@users.noreply.github.com>
2022-11-30 16:28:32 -0700

    Tweak env task (#603)

--- src/lib/infra/updateEnv.task.ts
+++ src/lib/infra/updateEnv.task.ts
@@ -1,4 +1,5 @@
 import type {Task} from '@feltcoop/gro';
+import {spawnProcess} from '@feltcoop/util/process.js';
 import {ENV_FILE_DEV, ENV_FILE_PROD, updateEnv} from '$lib/server/env';
 
 export const task: Task = {
@@ -6,10 +7,16 @@ export const task: Task = {
 	run: async ({fs, log, dev}) => {
 		const envFile = dev ? ENV_FILE_DEV : ENV_FILE_PROD;
 		const branch = (await fs.readFile('.git/HEAD', 'utf8')).trim().substring(5);
-		const [gitHashContents, currentEnvContents] = await Promise.all([
-			fs.readFile('.git/' + branch, 'utf8'),
-			fs.readFile(envFile, 'utf8'),
-		]);
+
+		let gitHashContents = '';
+		const buildResult = spawnProcess('git', ['show-ref', '-s', branch], {stdio: 'pipe'});
+		buildResult.child.stdout?.on('data', (data: Buffer) => {
+			gitHashContents += data.toString().trim();
+		});
+		await buildResult.closed;
+
+		if (gitHashContents === '') log.error('failed to load latest githash');
+		const currentEnvContents = await fs.readFile(envFile, 'utf8');
 		const gitHash = gitHashContents.trim().substring(0, 7);
 		const updatedEnvContents = updateEnv(currentEnvContents, 'PUBLIC_GIT_HASH', gitHash);
 		if (currentEnvContents !== updatedEnvContents) {
