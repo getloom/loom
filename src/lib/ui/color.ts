7f2b2d2d9847737ca3aaa7377855d87cc488f5b8
Ryan Atkinson <mail@ryanatkn.com>
2023-01-01 21:32:33 -0600

    add hue buckets for visual consistency (#653)

--- src/lib/ui/color.ts
+++ src/lib/ui/color.ts
@@ -1,5 +1,14 @@
 import {toRandomAlea} from '@feltcoop/util/random-alea.js';
 
+// TODO design a color scheme instead of calculating a number of evenly spaced buckets
+export const HUE_BUCKETS = 7;
+
 // TODO upstream to Felt -- need to figure out patterns with seed and swappable prng,
 // and refactor this to use a random int
-export const randomHue = (seed: any = ''): number => Math.round(toRandomAlea(seed)() * 360);
+export const randomHue = (seed: any = '', buckets: number | undefined = HUE_BUCKETS): number => {
+	const v = toRandomAlea(seed)() * 360;
+	if (buckets === undefined) {
+		return Math.round(v);
+	}
+	return Math.round(v - (v % (360 / buckets)));
+};
