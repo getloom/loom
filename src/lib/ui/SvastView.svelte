ca51e0f67e0c544847decf06a0318027182eafe8
Ryan Atkinson <mail@ryanatkn.com>
2023-01-12 16:13:42 -0700

    fix svast rendering for tags without children (#662)

--- src/lib/ui/SvastView.svelte
+++ src/lib/ui/SvastView.svelte
@@ -13,6 +13,7 @@
 	$: props = toViewProps(view) || EMPTY_OBJECT;
 </script>
 
+<!-- TODO can we format this without adding unwanted whitespace? -->
 <!-- TODO is using keyed each blocks ideal here? maybe create an id for each node? -->
 <!-- TODO should this render a fallback for unknown components? perhaps with a button to select a valid one? -->
 {#if view.type === 'svelteComponent'}{#if view.tagName in components}<svelte:component
@@ -20,8 +21,13 @@
 			{...props}
 			>{#each view.children as child (child)}<svelte:self view={child} />{/each}</svelte:component
 		>{:else}<code class="error-text">[unknown view: {view.tagName}]</code
-		>{/if}{:else if view.type === 'svelteElement'}<svelte:element this={view.tagName} {...props}
-		>{#each view.children as child (child)}<svelte:self view={child} />{/each}</svelte:element
-	>{:else if view.type === 'root'}{#each view.children as child (child)}<svelte:self
+		>{/if}{:else if view.type === 'svelteElement'}{#if view.children.length}<svelte:element
+			this={view.tagName}
+			{...props}
+			>{#each view.children as child (child)}<svelte:self view={child} />{/each}</svelte:element
+		>{:else}<svelte:element
+			this={view.tagName}
+			{...props}
+		/>{/if}{:else if view.type === 'root'}{#each view.children as child (child)}<svelte:self
 			view={child}
 		/>{/each}{:else if view.type === 'text'}{view.value}{/if}
