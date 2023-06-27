50937d6eb17d48fa44b43127c516c49659a0a230
Ryan Atkinson <mail@ryanatkn.com>
2023-06-27 14:39:30 -0600

    upgrade felt-ui (#832)

--- src/lib/ui/Settings.svelte
+++ src/lib/ui/Settings.svelte
@@ -1,12 +1,4 @@
 <script lang="ts">
-	import {writable} from '@feltcoop/svelte-gettable-stores';
-	import {
-		getTheme,
-		saveTheme,
-		type ColorScheme,
-		type Theme,
-		loadColorScheme,
-	} from '@feltjs/felt-ui/theme.js';
 	import {defaultThemes} from '@feltjs/felt-ui/themes.js';
 	import ColorSchemeInput from '@feltjs/felt-ui/ColorSchemeInput.svelte';
 	import ThemeInput from '@feltjs/felt-ui/ThemeInput.svelte';
@@ -17,27 +9,13 @@
 	const {
 		ui: {account},
 	} = getApp();
-
-	const colorScheme = writable<ColorScheme | null>(loadColorScheme());
-
-	const selectedTheme = getTheme();
-
-	const selectTheme = (theme: Theme): void => {
-		$selectedTheme = theme;
-		saveTheme(theme);
-	};
 </script>
 
 <div class="prose padded_xl box">
 	<h1>Settings</h1>
 	<h2>Color Scheme</h2>
-	<ColorSchemeInput {colorScheme} />
+	<ColorSchemeInput />
 	<h2>Theme</h2>
-	<ThemeInput
-		{selectedTheme}
-		themes={defaultThemes}
-		enableEditing={false}
-		on:select={(e) => selectTheme(e.detail)}
-	/>
+	<ThemeInput themes={defaultThemes} />
 	<AccountEditor {account} />
 </div>
