4c6045b6eeae42a08d291d8f1fd26abbf90dff6d
Ryan Atkinson <mail@ryanatkn.com>
2023-06-28 16:04:49 -0600

    add type to entity chip (#838)

--- src/lib/ui/EntityChip.svelte
+++ src/lib/ui/EntityChip.svelte
@@ -8,12 +8,12 @@
 	// TODO contextmenu?
 </script>
 
-<code class="entity_chip">{$entity.path || $entity.entity_id}</code>
+<code class="entity_chip">{$entity.data.type || 'Note'} {$entity.path || $entity.entity_id}</code>
 
 <style>
 	.entity_chip {
 		font-weight: 600;
 		font-size: var(--size_sm);
-		padding: var(--spacing_xs4);
+		padding: var(--spacing_xs4) var(--spacing_xs);
 	}
 </style>
