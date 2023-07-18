12947a53def89b9de52936454096b910cc47b489
Ryan Atkinson <mail@ryanatkn.com>
2023-07-18 17:19:30 -0600

    tweak vite config (#869)

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import {sveltekit} from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
-	optimizeDeps: {exclude: ['@feltjs/felt-ui']},
+	ssr: {noExternal: ['@feltjs/felt-ui']},
 	server: {
 		proxy: {
 			'/api': 'http://localhost:3000', // equal to `PUBLIC_API_SERVER_HOSTNAME + ':' + PUBLIC_API_SERVER_PORT`
