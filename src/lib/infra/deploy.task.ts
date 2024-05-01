e9b45f08517da6424ae2fd713cc8a585e09552b0
hamiltonjreed <hamiltonjreed@gmail.com>
2024-04-30 23:15:43 -0600

    gro format

--- src/lib/infra/deploy.task.ts
+++ src/lib/infra/deploy.task.ts
@@ -31,7 +31,7 @@ export const task: Task = {
 			SVELTEKIT_BUILD_DIRNAME,
 			'package.json',
 			'package-lock.json',
-			'svelte-gettable-stores-0.2.0.tgz',			
+			'svelte-gettable-stores-0.2.0.tgz',
 		];
 
 		// TODO ensure_dir helper? or omit the `exists` check?
