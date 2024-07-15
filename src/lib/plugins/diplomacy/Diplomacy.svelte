c3e1317f5d6ee7ad03531a902696ed96ae618975
greatbacon <hamiltonjreed@gmail.com>
2024-07-14 21:49:22 -0600

    Struct data & begin init

---
+++ src/lib/plugins/diplomacy/Diplomacy.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import type { Entity, EntityId } from "$lib/vocab/entity/entity";
+
+    type Country = "Austria-Hungary" | "Great Britain" | "France" | "Germany" | "Italy" | "Russia" | "Turkey" | "nuetral"
+
+    interface Territory {
+        name: string;
+        type: "land" | "sea";
+        owner: Country;
+        supply: boolean;
+        neighbors: EntityId[];
+        land_unit?: EntityId;
+        coast1?: EntityId;
+        coast2?: EntityId;
+        starting?: Country;
+    }
+
+
+    const territories: Territory[] = [
+        {
+            name: "Ankara",
+            type: "land",
+            owner: "Turkey",
+            supply: true,
+            neighbors: [],
+            coast1: 1
+        }
+    ]
+</script>
