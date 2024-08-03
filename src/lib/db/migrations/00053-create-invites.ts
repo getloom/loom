9c93fb793b81abc865e0e04193b6e42701275e73
greatbacon <hamiltonjreed@gmail.com>
2024-08-03 08:33:21 -0600

    Add initial migration & types

---
+++ src/lib/db/migrations/00053-create-invites.ts
@@ -0,0 +1,15 @@
+import type {Sql} from 'postgres';
+
+export const up = async (sql: Sql<any>): Promise<void> => {
+	await sql`
+	CREATE TABLE IF NOT EXISTS invites (
+		invite_id serial primary key,
+        code text,
+		from_id int REFERENCES accounts (account_id) ON UPDATE CASCADE ON DELETE CASCADE,
+        to_id int REFERENCES accounts (account_id) ON UPDATE CASCADE ON DELETE CASCADE,
+		status text,
+		created timestamp NOT NULL DEFAULT now(),
+		updated timestamp		
+	)
+	`;
+};
---
+++ src/lib/vocab/invite/inviteSchema.ts
@@ -0,0 +1,27 @@
+import type {Json_Schema} from '$lib/util/schema.js';
+
+export const InviteIdSchema = {
+	$id: '/schemas/InviteId',
+	type: 'number',
+	tsType: "Flavored<number, 'InviteId'>",
+	tsImport: "import {Flavored} from '@ryanatkn/belt/types.js'",
+} satisfies Json_Schema;
+
+export const InviteSchema = {
+	$id: '/schemas/Invite',
+	type: 'object',
+	description: `
+    Represents an invitation from one account to another for closed instances.
+	`,
+	properties: {
+		invite_id: {$ref: '/schemas/InviteId'},
+		code: {type: 'string'},
+		status: {type: 'string'},
+		from_id: {$ref: '/schemas/AccountId'},
+        to_id: {anyOf: [{$ref: '/schemas/AccountId'}, {type: 'null'}]},
+		created: {type: 'object', instanceof: 'Date'},
+		updated: {anyOf: [{type: 'object', instanceof: 'Date'}, {type: 'null'}]},
+	},
+	required: ['account_id', 'name', 'settings', 'password', 'created', 'updated'],
+	additionalProperties: false,
+} satisfies Json_Schema;
